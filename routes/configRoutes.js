4c6c36edf4d2e5d1f1d6bbecd7c83afbf448014d
superPantsXXXIII <124884690+superPantsXXXIII@users.noreply.github.com>
2023-03-16 00:07:46 +0200

    Add files via upload

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -10,6 +10,6 @@ exports.routesInit = (app) => {
   app.use("/rocks",RocksRouter);
 
   app.use((req,res)=>{
-    res.status(404).send('<h1>404 page not found ;-;</h1>')
+    res.status(404).send('<div style="flex-direction: column;height:100vh;width:100%;display:flex;align-items: center;justify-content: center;"><h1 style="font-size:2.5em">404 page not found ;-;</h1> </br> <button style="background-color: rgb(185, 122, 87);padding:24px;font-size:1em"><a href="https://ill-pear-angelfish-cap.cyclic.app/" style="text-decoration: none;color: white;">go back to Documentation</a></button></div>')
   })
 }
